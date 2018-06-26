713b4dc3cecddb6afd10840962e1fe3d82a01a30
Aaron Ang <awz.ang@gmail.com>
2018-06-25 22:18:10 -0700

    Delete random dependency

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-extern crate rand;
-
-use rand::Rng;
-
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
     println!("Hello, world!");
-    println!("Secrent number: {}!", secret_number);
     println!("Ok, world!");
     println!("Ok, world!");
     println!("Bye, world!");
