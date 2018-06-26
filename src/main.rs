bf8da1f2743174df1fa42815bfe87721823bfe00
Aaron Ang <awz.ang@gmail.com>
2018-06-25 22:02:39 -0700

    Generate secret number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
     println!("Hello, world!");
+    println!("Secrent number: {}!", secret_number);
     println!("Bye, world!");
 }
